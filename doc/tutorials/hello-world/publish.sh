93ef4902181bd55b6c310d1aa633770ee5187599
Jomi F. Hubner <jomifred@gmail.com>
2016-05-09 22:02:41 +0200

    add tutorial hello world

---
+++ doc/tutorials/hello-world/publish.sh
@@ -0,0 +1,8 @@
+USER=jomifred
+
+scp -r *.html $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
+
+#scp -r *.css $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
+#scp -r screens $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
+#scp -r solutions $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
+#scp -r code $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world

46a734dcfede4e7d941e7a7a1a7a45e28e761b22
Jomi F. Hubner <jomifred@gmail.com>
2016-10-17 09:51:41 -0200

    finish 0.6

--- doc/tutorials/coordination/publish.sh
+++ doc/tutorials/coordination/publish.sh
@@ -14,6 +14,6 @@ scp -r *.html $USERSF,jacamo@web.sf.net:$DEST
 #scp -r *.org $USERSF,jacamo@web.sf.net:$DEST
 
 #scp -r ../jcm.css $USERSF,jacamo@web.sf.net:$DEST/..
-#scp -r screens $USERSF,jacamo@web.sf.net:$DEST
-#scp -r solutions $USERSF,jacamo@web.sf.net:$DEST
+scp -r screens $USERSF,jacamo@web.sf.net:$DEST
+scp -r solutions $USERSF,jacamo@web.sf.net:$DEST
 scp -r code $USERSF,jacamo@web.sf.net:$DEST

9007ab8eeae9f4e696a969d5671ec3ba108b70ee
Jomi F. Hubner <jomifred@gmail.com>
2017-10-20 13:03:50 -0200

    add screen in shell doc

--- doc/tutorials/hello-world/publish.sh
+++ doc/tutorials/hello-world/publish.sh
@@ -9,6 +9,6 @@ asciidoctor shell-based.adoc
 scp -r *.html $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
 
 #scp -r *.css $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
-#scp -r screens $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
+scp -r screens $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
 #scp -r solutions $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
-scp -r code $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world
+#scp -r code $USER,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/tutorial/hello-world

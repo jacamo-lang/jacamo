3eae2ecfa72913e90c4fbf7d1299f551f89d39c4
cleberjamaral <cleberjamaral@gmail.com>
2020-05-13 09:28:02 -0300

    Adding tests for cartago

---
+++ src/test/java/TestUtils.java
@@ -0,0 +1,58 @@
+
+import jacamo.infra.JaCaMoLauncher;
+import jason.JasonException;
+import jason.infra.centralised.BaseCentralisedMAS;
+
+public final class TestUtils {
+    static Boolean systemRunning = false;
+    static BaseCentralisedMAS runner = null;
+    
+    public static void launchSystem(String jcm) {
+        if (!systemRunning) {
+            try {
+                // Launch jacamo and jacamo-rest running test0.jcm
+                new Thread() {
+                    public void run() {
+                        String[] arg = { jcm };
+                        try {
+                            JaCaMoLauncher.main(arg);
+                        } catch (JasonException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+
+                // get runner
+                while (runner == null) {
+                    System.out.println("waiting for the runner...");
+                    runner = JaCaMoLauncher.getRunner();
+                    Thread.sleep(200);
+                }
+                // wait for agents
+                while (JaCaMoLauncher.getRunner().getNbAgents() == 0) {
+                    System.out.println("waiting for agents to start...");
+                    Thread.sleep(200);
+                }
+                Thread.sleep(600);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            systemRunning = true;
+            //Runtime.getRuntime().addShutdownHook(new Thread(() -> stopSystem()));
+        }
+    }
+    
+    public static void stopSystem() {
+        JaCaMoLauncher.getRunner().finish(0, false); // do not stop the JVM
+        while (JaCaMoLauncher.getRunner() != null) {
+            System.out.println("waiting for jacamo to STOP ....");
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("JaCaMo stopped");
+    }
+
+}
---
+++ src/test/java/WorkspaceCreationTest.java
@@ -0,0 +1,111 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import cartago.AgentIdCredential;
+import cartago.ArtifactId;
+import cartago.ArtifactInfo;
+import cartago.ArtifactObsProperty;
+import cartago.CartagoContext;
+import cartago.CartagoException;
+import cartago.CartagoService;
+import cartago.Op;
+import cartago.WorkspaceId;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class WorkspaceCreationTest {
+
+    @BeforeClass
+    public static void launchSystem() {
+        TestUtils.launchSystem("src/test/java/project/p5.jcm");
+    }
+    
+    @Test
+    public void test101GetWorkspaces() {
+        while (CartagoService.getNode() == null) {
+            try {
+                System.out.println("waiting for cartago...");
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Testing if workspaces from jcm were really created.");
+        Collection<String> workspaces = CartagoService.getNode().getWorkspaces();
+        System.out.println("Existing workspaces: "+workspaces);
+        
+        assertTrue(workspaces.contains("w1"));
+        assertTrue(workspaces.contains("w2"));
+        assertTrue(workspaces.contains("w5"));
+    }
+    
+    @Test
+    public void test201PostProperty() {
+        System.out.println("Testing get observable property.");
+        
+        try {
+            Object[] props = getObsPropValue("w1", "c1", "count");
+            System.out.println("counter initial value: "+((Integer)props[0]).intValue());
+            assertEquals( 10, ((Integer)props[0]).intValue(), 0 );
+            
+            execOp("w1", "c1", "inc", null);
+            
+            props = getObsPropValue("w1", "c1", "count");
+            System.out.println("counter after inc: "+((Integer)props[0]).intValue());
+            assertEquals( 11, ((Integer)props[0]).intValue(), 0 );
+        } catch (CartagoException e) {
+            e.printStackTrace();
+        }
+    }
+        
+    @Test
+    public void test999Shutdown() {
+        System.out.println("Shutdown jacamo.");
+        TestUtils.stopSystem();
+    }
+    
+    public Object[] getObsPropValue(String wrksName, String artName, String obsPropId) throws CartagoException {
+        ArtifactInfo info = CartagoService.getController(wrksName).getArtifactInfo(artName);
+        for (ArtifactObsProperty op : info.getObsProperties()) {
+            if (op.getName().equals(obsPropId)) {
+                return op.getValues();
+            }           
+        }
+        return null;
+    }
+    Map<String, CartagoContext> contexts = new HashMap<>();
+
+    public void execOp(String wrksName, String artName, String operation, Object[] values) throws CartagoException {
+        CartagoContext ctxt = getContext(wrksName);
+        ArtifactId aid = ctxt.lookupArtifact(getWId(wrksName), artName);
+        if (aid == null) {
+            throw new CartagoException("artifact "+artName+" not found");
+        }
+        if (values != null) 
+            ctxt.doAction(aid, new Op(operation, values));
+        else
+            ctxt.doAction(aid, new Op(operation));
+    }
+    
+    protected CartagoContext getContext(String wrksName) throws CartagoException {
+        CartagoContext ctxt = contexts.get(wrksName);
+        if (ctxt == null) {
+            ctxt = CartagoService.startSession(wrksName, new AgentIdCredential("restapi_"+wrksName));
+            contexts.put(wrksName, ctxt);
+            ctxt.joinWorkspace( wrksName );
+        }
+        return ctxt;
+    }
+    
+    protected WorkspaceId getWId(String wrksName) throws CartagoException {
+        return getContext(wrksName).getJoinedWspId(wrksName); 
+    }
+}

6c30637ab515bef9b539989d0481ced50ef4f07b
Jomi Fred Hubner <jomifred@gmail.com>
2023-03-08 12:14:38 +0100

    Update publish.sh

--- src/main/resources/scripts/publish.sh
+++ src/main/resources/scripts/publish.sh
@@ -12,4 +12,8 @@ rm -rf .gradle
 zip -r ../npss.zip *
 cd ..
 
-scp *.zip $USERSF,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/nps
+#scp *.zip $USERSF,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/nps
+#### ******** USE branch site
+#### - select branch site
+#### - merge from develop or master or ...
+#### - run script publish.sh

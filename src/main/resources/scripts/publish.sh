b4e7341c8e85033b639cefef2ffcbcb8e16158f3
Jomi Fred Hubner <jomifred@gmail.com>
2023-03-09 11:02:04 +0100

    add nps

--- src/main/resources/scripts/publish.sh
+++ src/main/resources/scripts/publish.sh
@@ -2,10 +2,10 @@
 
 rm *.zip
 
-#cd jacamo-project-1.1
-#rm -rf .gradle
-#zip -r ../np1.1.zip *
-#cd ..
+# cd jacamo-project-1.1
+# rm -rf .gradle
+# zip -r ../np1.1.zip *
+# cd ..
 
 cd jacamo-project-ss
 rm -rf .gradle
@@ -16,6 +16,9 @@ cd ..
 #scp *.zip $USERSF,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/nps
 cp *.zip ../../../../docs/nps
 
+#git commit -m "new project script" ../../../../docs/nps/np1.1.zip
+
+git add ../../../../docs/nps/npss.zip
 git commit -m "new project script" ../../../../docs/nps/npss.zip
 git push
 

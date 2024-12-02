7a38a131d45719b65b21dea93501ca5defd5f401
Jomi Fred Hubner <jomifred@gmail.com>
2024-12-02 11:31:41 -0300

    update zips

--- doc/tutorials/coordination/publish.sh
+++ doc/tutorials/coordination/publish.sh
@@ -1,10 +1,3 @@
-# check user at sourceforge
-if [ -z $USERSF ] ; then
-    echo the var USERSF must be set with your username at source forge
-    exit
-fi
-
-DEST=/home/project-web/jacamo/htdocs/tutorial/coordination
 find . -name .DS_Store -exec rm {} \;
 
 # clean code and zip
@@ -22,11 +15,19 @@ docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/app "$IMAGE"
 
 mv readme.html index.html
 
-scp -r *.html $USERSF,jacamo@web.sf.net:$DEST
+# check user at sourceforge
+#if [ -z $USERSF ] ; then
+#    echo the var USERSF must be set with your username at source forge
+#    exit
+#fi
+
+#DEST=/home/project-web/jacamo/htdocs/tutorial/coordination
+
+#scp -r *.html $USERSF,jacamo@web.sf.net:$DEST
 #scp -r *.org $USERSF,jacamo@web.sf.net:$DEST
 
 #scp -r ../jcm.css $USERSF,jacamo@web.sf.net:$DEST/..
 #scp -r screens $USERSF,jacamo@web.sf.net:$DEST
 #scp -r solutions $USERSF,jacamo@web.sf.net:$DEST
-scp -r code $USERSF,jacamo@web.sf.net:$DEST
+#scp -r code $USERSF,jacamo@web.sf.net:$DEST
 #scp code/es.png $USERSF,jacamo@web.sf.net:$DEST/code
